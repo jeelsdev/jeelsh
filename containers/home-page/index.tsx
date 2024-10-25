69b6557d9f9b4070fd038ee4dc5bff3677d54809
Jeels <joelhuaman7m@gmail.com>
2024-10-24 23:37:04 -0500

    feat: about me

--- containers/home-page/index.tsx
+++ containers/home-page/index.tsx
@@ -1,18 +1,24 @@
 import Logo from "@/components/Logo/logo";
 import AvatarSocials from "./section-avatar-social";
+import About from "./section-about";
 
 const HomePage = () => {
     return (
-        <div className="w-full flex justify-between items-center border border-gray-600 rounded-sm p-4 dark:bg-slate-800">
-            <div className="flex items-center gap-1">
-                <Logo src="https://avatars.githubusercontent.com/u/130939421?v=4" alt="Jeelsh" />
-                <div className="font-medium dark:text-white ml-4">
-                    <div>Jeels</div>
-                    <div className="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+        <>
+            <div className="w-full flex justify-between items-center border border-gray-600 rounded-sm p-4 dark:bg-slate-800">
+                <div className="flex items-center gap-1">
+                    <Logo src="https://avatars.githubusercontent.com/u/130939421?v=4" alt="Jeelsh" />
+                    <div className="font-medium dark:text-white ml-4">
+                        <div>Jeels</div>
+                        <div className="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+                    </div>
                 </div>
+                <AvatarSocials />
             </div>
-            <AvatarSocials />
-        </div>
+
+            <About />
+            
+        </>
     );
 }
 
---
+++ containers/home-page/section-about/index.tsx
@@ -0,0 +1,20 @@
+const About = () => {
+    return (
+        <section className="about my-7">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="font-bold py-3">
+                            <h2>Acerca de mí</h2>
+                        </div>
+                        <div className="about-text text-justify">
+                            <p>Hola, soy Jeels (Joel) 😊, un apasionado desarrollador con un enfoque en el <span className="font-bold">Desarrollo Web</span>. Me encanta explorar tecnologías modernas y siempre estoy en busca de soluciones creativas. Actualmente, estoy perfeccionando mis habilidades en programación y aprendiendo inglés para expandir mis conocimientos y poder conectar con más desarrolladores 🚀.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
