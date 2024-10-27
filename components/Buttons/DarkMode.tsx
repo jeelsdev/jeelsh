b38d3f6f31e437a23a64a7f02b082a590432e595
Jeels <joelhuaman7m@gmail.com>
2024-10-26 23:53:34 -0500

    feat: dark mode button

--- components/Buttons/DarkMode.tsx
+++ components/Buttons/DarkMode.tsx
@@ -1,25 +1,60 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Theme from "../Avatars/Theme";
 
 const DarkMode = () => {
-    const [theme, setTheme] = useState("light");
+    const [activeTheme, setActiveTheme] = useState("system");
+    
+    useEffect(() => {
+        const theme = localStorage.getItem("theme");
+        if (!theme){
+            applySystemTheme();
+            setActiveTheme("system");
+        }else {
+            handleThemeChange(theme);
+            setActiveTheme(theme);
+        }
 
-    const toggleTheme = () => {
-        if (theme === "light") {
-            setTheme("dark");
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+        const handleSystemThemeChange = () => {
+            if(!theme || theme === "system") {
+                applySystemTheme();
+            }
+        }
+
+        mediaQuery.addEventListener("change", handleSystemThemeChange);
+        return () => {
+            mediaQuery.removeEventListener("change", handleSystemThemeChange);
+        }
+    }, []);
+
+    function applySystemTheme() {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark").matches;
+        if (systemTheme) {
             document.documentElement.classList.add("dark");
         } else {
-            setTheme("light");
             document.documentElement.classList.remove("dark");
         }
-    };
+    }
+
+    const handleThemeChange = (newTheme: string) => {
+        if (newTheme === "system") {
+            applySystemTheme();
+        } else if (newTheme === "dark") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+        }
+    }
 
     return (
         <button
             className="dark-mode border-2 border-gray-700 p-2 rounded-md hover:bg-gray-700"
-            onClick={toggleTheme}
+            onClick={() => handleThemeChange(activeTheme === "dark" ? "light" : "dark")}
         >
             <Theme />
         </button>
